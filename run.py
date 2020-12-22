4ff7d56505f66435881e6fa0280218e36535470a
Andrew Barnes <06abarne@googlemail.com>
2020-12-22 11:36:04 +0000

    Baseline gui now created and shows on run.

---
+++ run.py
@@ -0,0 +1,29 @@
+from tkinter import messagebox
+import pygubu
+
+
+
+class HelloWorldApp:
+
+    def __init__(self):
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file('ui.ui')
+        self.mainwindow = builder.get_object('frm_main')
+        # Set the toplevel window Not resizable
+        self.toplevel = self.mainwindow.winfo_toplevel()
+        self.toplevel.resizable(False, False)
+
+        builder.connect_callbacks(self)
+    
+    def btn_test_clicked(self):
+        messagebox.showinfo('Message', 'You clicked Button TEST')
+
+            
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+if __name__ == '__main__':
+    app = HelloWorldApp()
+    app.run()
